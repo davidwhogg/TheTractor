e6ab5cfe7f9e2870e8fc0f9f711f32b302ec51c9
David W. Hogg <dwhogg@gmail.com>
2012-08-03 14:25:51 +0200

    try to read pickle; failing

--- data/visualize_jd_maps.py
+++ data/visualize_jd_maps.py
@@ -8,11 +8,20 @@ if __name__ == "__main__":
     rc('text', usetex=True)
 import pylab as plt
 import pyfits
+import cPickle as pickle
 
 if __name__ == "__main__":
+    fd = open("dstn/herschel-100-g100b.pickle")
+    tractor = pickle.load(fd)
+    fd.close()
+    print tractor
+
+if False:
     jdimage = pyfits.open("jd/brick15.medianebv.fits")[0].data
     print jdimage.shape
+    print jdimage.min()
+    print jdimage.max()
     plt.gray()
     plt.clf()
-    plt.imshow(jdimage)
+    plt.imshow(jdimage, interpolation="nearest", vmin=0., vmax=jdimage.max())
     plt.savefig("jd_brick15.png")
