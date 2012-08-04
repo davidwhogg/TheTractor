32ebbc3a8f81c13cfa42398acd4670c25a6ba8c6
David W. Hogg <dwhogg@gmail.com>
2012-08-04 17:31:01 +0200

    kg image too

--- data/visualize_jd_maps.py
+++ data/visualize_jd_maps.py
@@ -6,22 +6,21 @@ if __name__ == "__main__":
     from matplotlib import rc
     rc('font',**{'family':'serif','serif':'Computer Modern Roman','size':12})
     rc('text', usetex=True)
+import numpy as np
 import pylab as plt
 import pyfits
 import cPickle as pickle
 
 if __name__ == "__main__":
-    fd = open("dstn/herschel-100-g100b.pickle")
-    tractor = pickle.load(fd)
-    fd.close()
-    print tractor
+    kgimage = pyfits.open("kg/av_map_b12.fits")[0].data
+    vmax = kgimage[np.where(np.isfinite(kgimage))].max()
+    plt.gray()
+    plt.clf()
+    plt.imshow(kgimage, interpolation="nearest", vmin=0., vmax=vmax)
+    plt.savefig("kg_b12.png")
 
-if False:
     jdimage = pyfits.open("jd/brick15.medianebv.fits")[0].data
-    print jdimage.shape
-    print jdimage.min()
-    print jdimage.max()
     plt.gray()
     plt.clf()
     plt.imshow(jdimage, interpolation="nearest", vmin=0., vmax=jdimage.max())
-    plt.savefig("jd_brick15.png")
+    plt.savefig("jd_b15.png")
