609cea1e507c75e5163f4aedf0f15bd862006597
David W. Hogg <dwhogg@gmail.com>
2012-09-27 19:34:32 +0000

    basic plotting of mixture results

--- code/plot_mixture_profiles.py
+++ code/plot_mixture_profiles.py
@@ -1,12 +1,16 @@
 '''
 This file is part of the Tractor project.
 Copyright 2011 David W. Hogg.
+
+### bugs:
+- duplicated code
+- brittle code
 '''
 
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import rc
-rc('font',**{'family':'serif','serif':'Computer Modern Roman','size':12})
+rc('font',**{'family':'serif','size':12})
 rc('text', usetex=True)
 import pylab as plt
 import numpy as np
@@ -18,17 +22,74 @@ import os as os
 models = ['exp', 'ser2', 'ser3', 'dev', 'ser5', 'lux', 'luv']
 Ks = [4, 6, 8, 10]
 
-def plot_mixtures_vs_n(K):
+def get_pars(model, K):
+    MR = 8
+    if model == 'lux':
+        MR = 4
+    picklefn = "%s_K%02d_MR%02d.pickle" % (model, K, MR)
+    picklefile = open(picklefn, "r")
+    pars = pickle.load(picklefile)
+    picklefile.close()
+    return pars
+
+def plot_mixtures_vs_model(K):
+    prefix = "mixtures_vs_model_K%02d" % K
+    plt.figure(figsize=(5,5))
+    plt.clf()
+    for model in models[0:5]:
+        pars = get_pars(model, K)
+        amps = pars[0:K]
+        sigmas = np.sqrt(pars[K:2 * K])
+        plt.plot(sigmas, amps, 'k-', alpha=0.5)
+        plt.plot(sigmas, amps, 'ko', ms=4.0)
+        label = model
+        tweak = 0.
+        if model == 'ser5': tweak = 5.
+        if model == 'dev': tweak = 1.
+        plt.annotate(label, [sigmas[0], amps[0]], xytext=[-2,-10], textcoords="offset points", va="baseline", ha="center")
+        plt.annotate(label, [sigmas[-1], amps[-1]], xytext=[4,-4+tweak], textcoords="offset points", va="baseline", ha="left")
+    plt.xlabel(r"root-variance $\sigma$ (units of half-light radii)")
+    plt.ylabel(r"amplitude")
+    plt.loglog()
+    plt.xlim(0.5e-4, 2.e1)
+    plt.ylim(0.5e-4, 2.e1)
+    hogg_savefig(prefix + ".png")
+    hogg_savefig(prefix + ".pdf")
     return None
 
 def plot_mixtures_vs_K(model):
+    prefix = "mixtures_vs_K_%s" % model
+    plt.figure(figsize=(5,5))
+    plt.clf()
+    for K in Ks:
+        pars = get_pars(model, K)
+        amps = pars[0:K]
+        sigmas = np.sqrt(pars[K:2 * K])
+        plt.plot(sigmas, amps, 'k-', alpha=0.5)
+        plt.plot(sigmas, amps, 'ko')
+        label = r"%d" % K
+        tweak = 0.
+        if model == 'dev': tweak = 4
+        plt.annotate(label, [sigmas[0], amps[0]], xytext=[-4,-4], textcoords="offset points", va="baseline", ha="right")
+        plt.annotate(label, [sigmas[-1], amps[-1]], xytext=[4,-4+tweak], textcoords="offset points", va="baseline", ha="left")
+    plt.xlabel(r"root-variance $\sigma$ (units of half-light radii)")
+    plt.ylabel(r"amplitude")
+    plt.loglog()
+    plt.xlim(0.5e-4, 2.e1)
+    plt.ylim(0.5e-4, 2.e1)
+    hogg_savefig(prefix + ".png")
+    hogg_savefig(prefix + ".pdf")
     return None
+
+def hogg_savefig(fn):
+    print "writing %s" % fn
+    return plt.savefig(fn)
 
 def make_all_plots():
     for model in models:
         plot_mixtures_vs_K(model)
     for K in Ks:
-        plot_mixtures_vs_n(K)
+        plot_mixtures_vs_model(K)
     return None
 
 if __name__ == "__main__":
