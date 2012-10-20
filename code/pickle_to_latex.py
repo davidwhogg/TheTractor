3caf2212f1145cb384cf496c1abf8e557b72ac3b
David W. Hogg <dwhogg@gmail.com>
2012-10-20 00:17:43 +0000

    works

--- code/pickle_to_latex.py
+++ code/pickle_to_latex.py
@@ -25,45 +25,48 @@ def get_pars(model, K):
     picklefile.close()
     return pars
 
-def write_table_line(strings):
+def write_table_line(fd, strings):
     endofline = r"&"
     for i,string in enumerate(strings):
         if i + 1 == len(strings): endofline = r"\\"
-        print r" %s %s" % (string, endofline)
+        fd.write(r" %s %s" % (string, endofline) + "\n")
     return None
 
 def write_table(model):
+    tablefn = r"table_%s.tex" % model
     if model == "exp" or model == "lux":
         Ks = [4, 6, 8]
     pars = [get_pars(model, K) for K in Ks]
     clist = "c|"
     for K in Ks: clist += "cc|"
-    print r"\begin{tabular}{%s}" % clist
-    print r"&"
-    print r"\multicolumn{%d}{|c|}{%s} \\" % (2 * len(Ks), model)
-    print r"\hline"
-    print r"$M^{\%s}=$ &" % model
-    write_table_line([r"\multicolumn{2}{|c|}{$%d$}" % K for K in Ks])
-    print r"$m$ &"
-    write_table_line([r"$a^{\%s}_m$ & $\sqrt{v^{\%s}_m}$" % (model, model) for K in Ks])
+    fd = open(tablefn, "w")
+    fd.write(r"\begin{tabular}{%s}" % clist + "\n")
+    fd.write(r"&" + "\n")
+    fd.write(r"\multicolumn{%d}{|c|}{%s} \\" % (2 * len(Ks), model) + "\n")
+    fd.write(r"\hline" + "\n")
+    fd.write(r"$M^{\%s}=$ &" % model + "\n")
+    write_table_line(fd, [r"\multicolumn{2}{|c|}{$%d$}" % K for K in Ks])
+    fd.write(r"$m$ &" + "\n")
+    write_table_line(fd, [r"$a^{\%s}_m$ & $\sqrt{v^{\%s}_m}$" % (model, model) for K in Ks])
     for k in range(np.max(Ks)):
-        print r"$%d$" % (k + 1)
+        fd.write(r"$%d$ &" % (k + 1) + "\n")
         aks = []
         svks = []
         for i,K in enumerate(Ks):
             if k < K:
                 aks += [r"%7.5f" % pars[i][k]]
-                svks += [r"%7.5f" % np.sqrt(pars[i][2 * k])]
+                svks += [r"%7.5f" % np.sqrt(pars[i][K + k])]
             else:
                 aks += [r"\cdots"]
                 svks += [r"\cdots"]
-        write_table_line([r"$%s$ & $%s$" % (a, sv) for a, sv in zip(aks, svks)])
-    print r"\hline"
-    print r"$\sum_m a^{\%s}_m=$ &" % model
-    write_table_line([r"\multicolumn{2}{|c|}{$%7.5f$}" % q for q in [np.sum(pars[i][0:K]) for i,K in enumerate(Ks)]])
-    print r"badness\,$=$ &"
-    write_table_line([r"\multicolumn{2}{|c|}{$%5.3f\times 10^{%d}$}" % (0, 0) for K in Ks])
-    print r"\end{tabular}{%s}"
+        write_table_line(fd, [r"$%s$ & $%s$" % (a, sv) for a, sv in zip(aks, svks)])
+    fd.write(r"\hline" + "\n")
+    fd.write(r"$\sum_m a^{\%s}_m=$ &" % model + "\n")
+    write_table_line(fd, [r"\multicolumn{2}{|c|}{$%7.5f$}" % q for q in [np.sum(pars[i][0:K]) for i,K in enumerate(Ks)]])
+    fd.write(r"badness\,$=$ &" + "\n")
+    write_table_line(fd, [r"\multicolumn{2}{|c|}{$%5.3f\times 10^{%d}$}" % (0, 0) for K in Ks])
+    fd.write(r"\end{tabular}" + "\n")
+    fd.close()
     return None
 
 if __name__ == "__main__":
